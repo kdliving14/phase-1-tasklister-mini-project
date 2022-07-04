5c54c28cbce4145ab2eb65d0480a6fcbb731e64e
kdliving14 <105955246+kdliving14@users.noreply.github.com>
2022-07-04 12:23:49 -0700

    Update index.js
    was not being pushed from computer, manual fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => 
+{
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>
+  {
+    e.preventDefault();
+    mkDo(e.target.newTaskDescription.value)
+    form.reset()
+  })
+
+  function mkDo(item)
+  {
+    console.log(item)
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+
+    btn.addEventListener('click', del);
+    btn.textContent = 'x'
+
+    p.textContent = `${item}`;
+    p.appendChild(btn);
+
+    document.querySelector('ul').appendChild(p);
+
+  }
+
+  function del(event)
+  {
+    event.target.parentNode.remove()
+  }
+  
 });
